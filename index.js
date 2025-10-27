8847928813ec323e4c4bd5770eeb43e8f6973ad4
Tone Engel <tone@kizmet.org>
2025-10-27 15:34:52 -0400

    convert to redirect only

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-const { startChaintracksService, ChaintracksService } = require("@cwi/chaintracks-core")
+const https = require('https');
+const fs = require('fs');
 require('dotenv').config()
 
 const chain = process.env.CHAIN
 
 const options = {
-	...ChaintracksService.createChaintracksServiceOptions(),
 	httpsPrivateKeyPath: '/home/headersync/certs/privkey.pem',
 	httpsCertificatePath: '/home/headersync/certs/fullchain.pem'
 }
-debugger;
-// HTTPS
-const servicePromise = startChaintracksService(chain, options)
-// HTTP
-//const servicePromise = startChaintracksService(chain)
+
+// SSL certificate paths (replace with your actual certificate paths)
+const privateKey = fs.readFileSync(options.httpsPrivateKeyPath, 'utf8');
+const certificate = fs.readFileSync(options.httpsCertificatePath, 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+// Create HTTPS server
+const server = https.createServer(credentials, (req, res) => {
+  // Redirect all requests to the target domain
+  res.writeHead(301, {
+    Location: `https://${chain}net-chaintracks.babbage.systems${req.url}`
+  });
+  res.end();
+});
+
+// Listen on port 443 (default HTTPS port)
+const PORT = 443;
+server.listen(PORT, () => {
+  console.log(`HTTPS redirect server running on port ${PORT}`);
+});
